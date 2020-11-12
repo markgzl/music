a1d7719a095f0fe3bce12b6bd626dc711dad3601
guozilin <728109508@qq.com>
2020-11-12 21:28:07 +0800

    singer list

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,36 @@
 import { useEffect, useState } from 'react'
 import HorizenScroll from '../../baseUI/horizen-item'
 import { categoryTypes, alphaTypes } from '../../api/config'
-import { CatScrollWrap } from './style'
+import { CatScrollWrap, SingerListWrap, SingerBox } from './style'
+import Scroll from '../../baseUI/scroll'
+
+const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map (item => {
+	return {
+	  picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+	  name: "隔壁老樊",
+	  accountId: 277313426,
+	  key: item
+	}
+  }); 
+
+function renderSingerList(list){
+	return (
+		<div>
+			{
+				list.map(singer=>(
+					<SingerBox key={singer.key}>
+						<div className='img-wrap'>
+							<img src={singer.picUrl} width='100%' height='100%' alt={singer.name} />
+						</div>
+						<div className='name'>{singer.name}</div>
+					</SingerBox>
+				))
+			}
+		</div>
+	)
+}
+
+  
 function Singers(){
 	const [ cat, setCat ] = useState('')
 	const [ alpha, setAlpha ] = useState('')
@@ -21,6 +50,11 @@ function Singers(){
 				<HorizenScroll curVal={cat} list={categoryTypes} title={'分类(默认分类):'} onItemClick={handleCatClick}  />
 				<HorizenScroll curVal={alpha} list={alphaTypes} title={'歌手首字母:'} onItemClick={handleAlphaClick} />
 			</CatScrollWrap>
+			<SingerListWrap>
+				<Scroll>
+					{renderSingerList(singerList)}					
+				</Scroll>
+			</SingerListWrap>
 
 		</div>
 	
--- src/application/Singers/style.js
+++ src/application/Singers/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import style from '../../assets/global-style'
 export const CatScrollWrap = styled.div`
 	position: fixed;
 	top: 94px;
@@ -6,5 +7,35 @@ export const CatScrollWrap = styled.div`
 	width: 100%;
 	box-sizing: border-box;
 	overflow: hidden;
+`
+
+export const SingerListWrap = styled.div`
+	position: fixed;
+	top: 150px;
+	bottom: 0;
+	width: 100%;
+	box-sizing: border-box;
+	overflow: hidden;
+	padding-top: 15px;
+`
+export const SingerBox = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	padding: 10px 0 10px 10px;
+	border-bottom: 1px solid ${style["border-color"]};
+  	.img-wrap {
+		margin-right: 20px;
+		img {
+			border-radius: 3px;
+			width: 50px;
+			height: 50px;
+		}
+	}
+	.name {
+		font-size: ${style["font-size-m"]};
+		color: ${style["font-color-desc"]};
+		font-weight: 500;
+	}
 
 `
