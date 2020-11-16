d128fc7cb56e069189d9b40c0c484a87cad633d8
guozilin <728109508@qq.com>
2020-11-16 18:07:06 +0800

    recommend detail

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,6 +1,6 @@
 import React, { memo, useState, useRef } from 'react'
 import { CSSTransition } from 'react-transition-group'
-import { Container } from './style'
+import { Container,Top, Action } from './style'
 import Header  from '../../baseUI/header'
 
 const Album = (props) => {
@@ -13,7 +13,39 @@ const Album = (props) => {
 		<CSSTransition in={showStatus} timeout={300} classNames='fly' appear={true} onExited={props.history.goBack} unmountOnExit>
 			<Container>
 				<Header title={'返回'} handleClick={handleBack} ref={transitionRef} />
-				<button onClick={handleBack}>back</button>
+				<Top>
+					<div>
+						<div className='img-wrap'>
+							<img />
+							<div className='play'></div>
+						</div>
+						<div className='song-desc'>
+							<h2>sa</h2>
+							<div>
+								<img />
+								<div></div>
+							</div>
+						</div>
+					</div>
+					<Action>
+						<div className='action-item'>
+							<div><i className="iconfont">&#xe6ad;</i></div>
+							<div>评论</div>
+						</div>
+						<div className='action-item'>
+							<div><i className="iconfont">&#xe86f;</i></div>
+							<div>点赞</div>
+						</div>
+						<div className='action-item'>
+							<div><i className="iconfont">&#xe62d;</i></div>
+							<div>收藏</div>
+						</div>
+						<div className='action-item'>
+							<div><i className="iconfont">&#xe606;</i></div>
+							<div>更多</div>
+						</div>
+					</Action>
+				</Top>
 			</Container>
 		</CSSTransition>
 	)
